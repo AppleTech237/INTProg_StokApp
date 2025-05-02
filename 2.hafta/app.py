4a704acbc573eaa91e19830ac789e96192cf3b4c
AppleTech237 <86877090+AppleTech237@users.noreply.github.com>
2025-05-02 14:36:25 +0300

    Update app.py

--- 2.hafta/app.py
+++ 2.hafta/app.py
@@ -35,4 +35,6 @@ def urun_ayrintilari():
     return render_template("urun_ayrintilari.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
